5fd4c609e42e51ca93f6b610507ac95506a58f12
Bruce Johnston <bjohnsto@redhat.com>
2023-04-11 15:03:54 -0400

    Update index to set reviewers

--- dist/index.js
+++ dist/index.js
@@ -35356,78 +35356,115 @@ const _ = __nccwpck_require__(250);
 const context = github.context;
 
 const parseConfig = function (content) {
-  var r = yaml.parse(content);
-  console.log(r);
-  if(r == []) {
-    throw "config is empty or unparseable"
-  }
-  return r;
+	var r = yaml.parse(content);
+	console.log(r);
+	if(r == []) {
+		throw "config is empty or unparseable"
+	}
+	return r;
 };
 
 async function fetchContent(client) {
-  const response = await client.rest.repos.getContent({
-    owner: "TheSafetyDanceFanClub",
-    repo: "vdo-auto-assign",
-    path: ".github/auto_assign.yml"
-  });
+	const response = await client.rest.repos.getContent({
+		owner: "TheSafetyDanceFanClub",
+		repo: "vdo-auto-assign",
+		path: ".github/auto_assign.yml"
+	});
 
-  return Buffer.from(response.data.content, response.data.encoding).toString();
+	return Buffer.from(response.data.content, response.data.encoding).toString();
 };
 
+async function assignReviewer(octokit, reviewer) {
+	let key = "reviewers";
+	let target = reviewer;
+
+	if (_.isObject(reviewer) && _.has(reviewer, "team")) {
+		key = "team_reviewers";
+		target = reviewer.team;
+	}
+
+	await octokit.pulls.requestReviewers({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		pull_number: context.payload.pull_request.number,
+		[key]: [target],
+	});
+}
+
+async function assignAssignee(octokit, assignee) {
+	let key = "assignees";
+	let target = assignee;
+
+	if (_.isObject(assignee) && _.has(assignee, "team")) {
+			return;
+	}
+
+	await octokit.pulls.addAssignees({
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		pull_number: context.payload.pull_request.number,
+		[key]: [target],
+	});
+}
+		
 const main = async () => {
-  try {
-    /**
-     * We need to fetch all the inputs that were provided to our action
-     * and store them in variables for us to use.
-     **/
-    const token = core.getInput('token', { required: true });      
+	try {
+		/**
+		 * We need to fetch all the inputs that were provided to our action
+		 * and store them in variables for us to use.
+		 **/
+		const token = core.getInput('token', { required: true });		 
 
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
-    const octokit = new github.getOctokit(token);
+		/**
+		 * Now we need to create an instance of Octokit which will use to call
+		 * GitHub's REST API endpoints.
+		 * We will pass the token as an argument to the constructor. This token
+		 * will be used to authenticate our requests.
+		 * You can find all the information about how to use Octokit here:
+		 * https://octokit.github.io/rest.js/v18
+		 **/
+		const octokit = new github.getOctokit(token);
 
-    // Now we parse the config file.
-    const configContent = await fetchContent(octokit);
-    const config = parseConfig(configContent);
+		// Now we parse the config file.
+		const configContent = await fetchContent(octokit);
+		const config = parseConfig(configContent);
 
-    core.debug("config");
-    core.debug(JSON.stringify(config));
+		core.debug("config");
+		core.debug(JSON.stringify(config));
 
-    const { data: pullRequest } = await octokit.rest.pulls.get({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      pull_number: context.payload.pull_request.number,
-    });
-      
-    const author = pullRequest.user.login;
-      
-    var reviewers = new Set();
-    _.forEach(_.keys(config), (globPattern) => {
-      core.info(globPattern);
-      let newReviewers = _.pull(config[globPattern], author);
-      for (const reviewer of newReviewers) {
-        reviewers.add(reviewer);
-      }
-    });      
+		const { data: pullRequest } = await octokit.rest.pulls.get({
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			pull_number: context.payload.pull_request.number,
+		});
+			
+		const author = pullRequest.user.login;
 
-    core.debug("reviewers");
-    core.debug(reviewers);
-      
-    core.info("finished!");
-      
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+		// Authors cannot be reviewers
+		let reviewer = config["czar"];
+		if (config["czar"] == author) {
+			reviewer = config["backup"];
+		}
+			
+		assignReviewer(octokit, reviewer).catch((error) => {
+			core.setFailed(error.message);
+		});
+			
+		assignAssignee(octokit, author).catch((error) => {
+			core.setFailed(error.message);
+		});
+			
+		core.info("finished!");
+			
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
 // Call the main function to run the action
 main();
+
+		
 
 })();
 
